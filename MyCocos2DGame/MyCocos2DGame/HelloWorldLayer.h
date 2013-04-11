8655351fa8f682e7727a02b7b24e80a4929185e5
andyque <guanghui8827@126.com>
2013-04-11 21:16:51 +0800

    add two projects

---
+++ MyCocos2DGame/MyCocos2DGame/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  MyCocos2DGame
+//
+//  Created by Ricky on 11/5/12.
+//  Copyright eseedo 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
