530365d3ec1a28c19e7d30da070c489131caa2eb
andyque <guanghui8827@126.com>
2013-04-11 18:43:25 +0800

    init

---
+++ HelloCocos2D/HelloCocos2D/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloCocos2D
+//
+//  Created by guanghui on 9/2/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
